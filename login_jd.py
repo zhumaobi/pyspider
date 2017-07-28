41a818bdfdcb7a74444303606ca0a03cad22ee39
zhumaobi <zhumaobi@qq.com>
2017-07-28 16:29:19 +0800

    模拟登陆
    #######################
    csdn,京东，qq空间和知乎的selenium模拟登陆实现，测试版本位python2.7.4。webdriver使用的是chromedriver，当然selenium也支持firefox,safari等浏览器，但本程序没有亲测。
    #######################

---
+++ login_jd.py
@@ -0,0 +1,36 @@
+##!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+from selenium import webdriver
+
+
+def login(login_url, login_name, login_passwd):
+    driver = webdriver.Chrome('/Users/maobizhu/Documents/python_codes/chromedriver')
+    driver.get(login_url)
+    time.sleep(5)
+
+    login_tab_right = driver.find_element_by_class_name('login-tab-r')
+    login_tab_right.click()
+
+    account = driver.find_element_by_id('loginname')
+    password = driver.find_element_by_id('nloginpwd')
+    submit = driver.find_element_by_id('loginsubmit')
+
+    account.clear()
+    password.clear()
+    account.send_keys(login_name)
+    password.send_keys(login_passwd)
+
+    submit.click()
+    time.sleep(5)
+
+    jd_cookies = driver.get_cookies()
+    driver.close()
+    return jd_cookies
+
+if __name__ == '__main__':
+    url = 'https://passport.jd.com/new/login.aspx'
+    name = raw_input('请输入用户名:\n')
+    password = raw_input('请输入密码:\n')
+    cookies = login(url, name, password)
+    print(cookies)
